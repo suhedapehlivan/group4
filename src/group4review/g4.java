6eee8152607a57cf970d3811273f0d9937a32b24
SuhedaPehlivan <suheda.90@me.com>
2022-12-12 22:13:17 +0300

    update g4

--- src/group4review/g4.java
+++ src/group4review/g4.java
@@ -4,5 +4,6 @@ public class g4 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("bye");
+        System.out.println("oke");
     }
 }
